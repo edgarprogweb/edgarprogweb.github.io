0899b55379822387724af7d2a2f7ec4e4f7f4c54
edgarprogweb <84821075+edgarprogweb@users.noreply.github.com>
2021-05-27 00:53:53 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCUnMy559SD__6hl_Qr3mYrz70xh4vOxHM",
+    authDomain: "edgarprogwebb.firebaseapp.com",
+    projectId: "edgarprogwebb",
+    storageBucket: "edgarprogwebb.appspot.com",
+    messagingSenderId: "131431799947",
+    appId: "1:131431799947:web:942be6334599c0363f088e",
+    measurementId: "G-MZY2VCW2BM"
+  });
